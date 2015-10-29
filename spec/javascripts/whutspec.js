583ef1bb6d8308aac9126231a910f100695d0f65
otron <oddweb42+gith@gmail.com>
2015-10-29 16:25:12 +0100

    Add single jasmine test
    * a bunch of settings are decided by
      `spec/javascripts/support/jasmine.yml`
      Like where to look for spec files and the pattern their names
      must conform to.
    
    * run jasmine after selenium tests

---
+++ spec/javascripts/whutspec.js
@@ -0,0 +1,9 @@
+describe("This is a group of test specs.", function() {
+    it("is a test spec.", function() {
+
+        // Keepin' it simple.
+        expect(true).toBe(!(false));
+
+    });
+
+});
