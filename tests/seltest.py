a8f40ae4469b4bda29a0b4ebc7e21e897892282d
otron <oddweb42+gith@gmail.com>
2015-11-02 17:04:39 +0100

    Fix selenium tests to run on Travis
    * run selenium tests with `py.test` instead of `python`
      * Turns out the tests weren't actually run!
    
    * Changes to/in `seltest.py`:
      * Typo/error: replace call to `webdriver.get_element_...`
      with `webdriver.find_element_...`
      * Use PhantomJS instead of Firefox. Getting it to work with Firefox
      (or any non-headless browser) requires setting up xvfb. We'll do that
      later.
      * Add an assert to check the page url after loading
      * Specify full URL to test page

--- tests/seltest.py
+++ tests/seltest.py
@@ -3,9 +3,11 @@ import pytest
 
 
 def test_selenium_basic():
-    driver = webdriver.Firefox()
-    driver.get("localhost:5000")
-    bod = driver.get_element_by_tag_name('body')
+    driver = webdriver.PhantomJS()
+    urlstr = "http://localhost:5000/"
+    driver.get(urlstr)
+    assert driver.current_url == urlstr
+    bod = driver.find_element_by_tag_name('body')
     assert "Hello" in bod.text
 
     driver.quit()
