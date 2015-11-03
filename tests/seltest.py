87dc851796d7504eec1e716153454e2d4fabbd9f
otron <oddweb42+gith@gmail.com>
2015-11-03 10:03:32 +0100

    Run selenium tests in Firefox on Travis
    * Browser is specified via the environment variable
      `SELTEST_BROWSER`. Defaults to PhantomJS.
    
    * Selenium/webdriver tests now run w/ Firefox on Travis-CI.

--- tests/seltest.py
+++ tests/seltest.py
@@ -1,9 +1,22 @@
 from selenium import webdriver
 import pytest
+import os
+
+
+def init_browser_via_env_var(env_str='SELTEST_BROWSER'):
+    """Returns a webdriver instance of the browser specified by the
+    `env_str` arg."""
+    try:
+        browser_nm = os.environ[env_str]
+        # os.environ[key] raises a KeyError if the env. var doesn't exist
+    except KeyError:
+        browser_nm = 'PhantomJS'
+    finally:
+        return getattr(webdriver, browser_nm)()
 
 
 def test_selenium_basic():
-    driver = webdriver.PhantomJS()
+    driver = init_browser_via_env_var()
     urlstr = "http://localhost:5000/"
     driver.get(urlstr)
     assert driver.current_url == urlstr
